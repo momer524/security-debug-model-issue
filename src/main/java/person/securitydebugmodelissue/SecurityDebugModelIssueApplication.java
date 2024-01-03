c3d31956708df0c1acfa96686351150999629660
Cafe <mzr13129712550@163.com>
2024-01-03 21:43:27 +0800

    init

---
+++ src/main/java/person/securitydebugmodelissue/SecurityDebugModelIssueApplication.java
@@ -0,0 +1,13 @@
+package person.securitydebugmodelissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDebugModelIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDebugModelIssueApplication.class, args);
+    }
+
+}
